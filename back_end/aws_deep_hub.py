28ff0dd6c7a69587774271dcb2de972112c15cdf
chris.evi-parker <chrisparker126@gmail.com>
2018-12-11 12:00:55 +0000

    modified to deal with attaching and mounting multiple file volumes

--- back_end/aws_deep_hub.py
+++ back_end/aws_deep_hub.py
@@ -1,6 +1,7 @@
 import argparse
 import yaml
 import boto3
+import botocore
 import webbrowser
 import functools
 import random
@@ -47,9 +48,15 @@ def launch_spot_instance(spot_price, ami, key_name, instance_type, security_grou
         raise e
 
 def check_spot_request(request_id):
-    client = boto3.client('ec2')
-    response = client.describe_spot_instance_requests(SpotInstanceRequestIds=[request_id])
-    return response['SpotInstanceRequests'][0]['Status']['Code'] == 'fulfilled'
+    try:
+        client = boto3.client('ec2')
+        response = client.describe_spot_instance_requests(SpotInstanceRequestIds=[request_id])
+        return response['SpotInstanceRequests'][0]['Status']['Code'] == 'fulfilled'
+    except botocore.exceptions.ClientError as e:
+        # not found, but may still appear in time ...
+        if e.response['Error']['Code'] == 'InvalidSpotInstanceRequestID.NotFound':
+            return False
+        raise e
 
 def get_spot_request_instance_id(request_id):
     client = boto3.client('ec2')
@@ -59,9 +66,9 @@ def get_spot_request_instance_id(request_id):
     return None
     
     
-def attach_volume(instance_id, volume_id):
+def attach_volume(device_name, instance_id, volume_id):
     client = boto3.client('ec2')
-    response = client.attach_volume(Device='/dev/xvdh', InstanceId=instance_id, VolumeId=volume_id)
+    response = client.attach_volume(Device=device_name, InstanceId=instance_id, VolumeId=volume_id)
     return response['ResponseMetadata']['HTTPStatusCode'] == 200
         
 def check_volume(volume_id):    
@@ -89,10 +96,14 @@ def launch_nb_browser(instance_id):
     webbrowser.open(address)
 
 def check_cmd(command_id, instance_id):
-    ssm_client = boto3.client('ssm')
-    cmi = ssm_client.get_command_invocation(CommandId=command_id, \
+    try:
+        ssm_client = boto3.client('ssm')
+        cmi = ssm_client.get_command_invocation(CommandId=command_id, \
                                             InstanceId=instance_id)
-    return cmi['Status'] == 'Success'
+        return cmi['Status'] == 'Success'
+    except botocore.exceptions.ClientError as e:
+        print('Check command error code: ', e.response['Error']['Code'])
+    return False                                                
 
 def is_instance_running(instance_id):
     client = boto3.client('ec2')
@@ -109,11 +120,12 @@ def func_spin(func, timeout=5, sleep_interval=0.3):
         else:
             time.sleep(sleep_interval)    
 
-def is_instance_visible_to_ssm(instance_id):
+def is_instance_visible_to_ssm(instance_id):    
     ssm_client = boto3.client('ssm')
     response = ssm_client.describe_instance_information(InstanceInformationFilterList=[{ 'key' : 'InstanceIds', \
-                                                                                    'valueSet' : [instance_id]}])
+                                                                                'valueSet' : [instance_id]}])
     return len(response['InstanceInformationList']) == 1
+
 
 def main():    
     global arg
@@ -144,56 +156,71 @@ def main():
     
     # spin and check when its fulfilled 
     check = functools.partial(is_instance_running, instance_id)
-    ret = func_spin(check, timeout=10)
+    ret = func_spin(check, timeout=40)
     
     if not ret:
         print('instance does not seem to be running!')
-        return             
+        return                     
         
-    print('Attaching volume: ', config['volume_id'])
+    volumes = config['volumes']
     
-    # then attach specified volume
-    attach_volume(instance_id, config['volume_id'])
-    
-    # then check volume is attached
-    check = functools.partial(check_volume, config['volume_id'])
-    
-    ret = func_spin(check, timeout=10)
-    
-    if not ret:
-        print('volume attach failed!')
-        return 
+    if len(volumes) == 0:
+        print('not volumes given to attach!')
+        return
+        
+    devices = []
+    for volume in volumes:
+        for volume_name in volume:
+            volume_data = volume[volume_name]
+            print('Attaching volume: ', volume_name)
+            
+            volume_id = volume_data['id']
+            aws_device_name = volume_data['aws_device_name']
+            
+            # then attach specified volume
+            attach_volume(aws_device_name, instance_id, volume_id)
+        
+            # then check volume is attached
+            check = functools.partial(check_volume, volume_id)
+        
+            ret = func_spin(check, timeout=15)
+        
+            if not ret:
+                print('volume attach failed!')
+                return 
+            else:
+                devices.append((volume_data['device'],volume_data['mount_point']))
 
-    # then check volume is attached
+    # check ssm is running 
     check = functools.partial(is_instance_visible_to_ssm, instance_id)
     
-    ret = func_spin(check, timeout=60, sleep_interval=1.5)
+    ret = func_spin(check, timeout=80, sleep_interval=1.5)
     
     if not ret:
         print('instance not found by ssm!')
         return 
     
-    device = None
-    if 'p' in config['instance_type']:
-        device = '/dev/xvdf'
-    elif 'm5d' in config['instance_type']:
-        device = '/dev/nvme2n1'
-    elif 'm5' in config['instance_type']:
-        device = '/dev/nvme1n1'
+    if len(devices) == 0:
+        print('no devices to mount!')
+        return
+        
+    # mount devices 
+    for d in devices:
+        device = d[0]
+        mount_point = d[1]
+        print('mounting {}, to point {} for instance {}'.format(device, mount_point, instance_id))
     
-    mount_point = '/home/ubuntu/work'
-    print('mounting {}, to point {} for instance {}'.format(device, mount_point, instance_id))
-    
-    command_id = mount_volume(instance_id, device, mount_point)
+        command_id = mount_volume(instance_id, device, mount_point)
 
-    # then check volume is attached
-    check = functools.partial(check_cmd, command_id, instance_id)
+        # check mount succeeded
+        check = functools.partial(check_cmd, command_id, instance_id)
     
-    ret = func_spin(check, timeout=10)
+        ret = func_spin(check, timeout=10, sleep_interval=1.0)
     
-    if not ret:
-        print('mount failed!')
-        return 
+        # if mount fails for 'm5d' mount type try 1n1 
+        if not ret:
+            print('mount failed for ', device)
+      
     
     launch_nb_browser(instance_id)
     
